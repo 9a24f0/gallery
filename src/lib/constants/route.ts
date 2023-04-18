9acf739b6308e103b78e37c0fa6df335eb51333e
Ngô Xuân Minh <xminh.ngo.00@gmail.com>
2023-04-18 23:07:46 +0700

    Upload photos

--- src/lib/constants/route.ts
+++ src/lib/constants/route.ts
@@ -1,5 +1,7 @@
 export const routes = [
-  { id: '01', date: 'Feb, 2023', coverImg: '/post/01/cafe.jpg'},
-  { id: '02', date: 'Mar, 2023', coverImg: '/post/02/clouds.jpg' },
-  { id: '03', date: 'Apr, 2023', coverImg: '/post/03/eclipse.jpg'},
+  { id: '01', title: 'Sapa 21\'', coverImg: '/post/01/buddha-statue.jpg'},
+  { id: '02', title: '2022', coverImg: '/post/02/after-rain.jpg' },
+  { id: '03', title: 'Jan, 2023', coverImg: '/post/03/vegetables.jpg'},
+  { id: '04', title: 'Feb, 2023', coverImg: '/post/04/cháu.jpg'},
+  { id: '05', title: 'Apr, 2023', coverImg: '/post/05/red.jpg'},
 ]
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,11 +10,11 @@
   </div>
   <div class="flex flex-col">
     <div class="bg-sky-100 m-16 text-center py-8 text-2xl rounded-xl">
-      Hello, this is mingnho's hault where he kept the photos he's not so ashamed of.
+      Hello, this is where mingnho kept the photos he's not so ashamed of!
     </div>
     <div class="w-full px-16 flex-col">
       {#each routes as route}
-        <ImageCard title={route.date} coverImgUrl={route.coverImg} path={route.id}></ImageCard>
+        <ImageCard title={route.title} coverImgUrl={route.coverImg} path={route.id}></ImageCard>
       {/each}
     </div>
   </div>
